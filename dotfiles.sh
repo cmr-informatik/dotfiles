b11ab0c0d86ed98b782905d5ca5d5a3f7be22d45
Christopher M. Riedl <cmr@informatik.wtf>
2019-09-06 15:14:14 -0500

    Separate directories with a space for stow
    Signed-off-by: Christopher M. Riedl <cmr@informatik.wtf>

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-for dir in $(find . -maxdepth 1 -type d -not -name '\.*' -printf '%P'); do
+for dir in $(find . -maxdepth 1 -type d -not -name '\.*' -printf '%P '); do
 	stow --dotfiles --verbose "$dir"
 done
